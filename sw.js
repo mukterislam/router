cd237c6879297ed4227a64c3ff53b1b3f2436a71
mukterislam <70107450+mukterislam@users.noreply.github.com>
2026-04-28 21:43:49 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'router-v2';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
